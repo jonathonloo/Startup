66276fa939dbb1b8c81ca4ab81390afb01539e50
Jonathon Loo <jloo@referentia.com>
2012-04-28 16:48:42 -1000

    Minor layout changes

--- Startup/src/com/wobuddy/activity/ActionSelectActivity.java
+++ Startup/src/com/wobuddy/activity/ActionSelectActivity.java
@@ -1,6 +1,7 @@
 package com.wobuddy.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,25 +14,38 @@ public class ActionSelectActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.actionselect);
-		
+
 		Button scanButton = (Button) findViewById(R.id.scanButton);
 		scanButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-//				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-//				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-//				startActivityForResult(intent, 0);
 
+			public void onClick(View v) {
+				Intent intent = new Intent(
+						"com.google.zxing.client.android.SCAN");
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, 0);
 			}
 		});
-		
+
 		Button selectButton = (Button) findViewById(R.id.selectButton);
 		selectButton.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent intent = new Intent("com.wobuddy.activity.MACHINELIST");
+				startActivityForResult(intent, 0);
 			}
 		});
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				Button button = (Button) findViewById(R.id.scanButton);
+				button.setText(contents);
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+			}
+		}
 	}
 }
--- Startup/src/com/wobuddy/activity/WOBuddyActivity.java
+++ Startup/src/com/wobuddy/activity/WOBuddyActivity.java
@@ -13,22 +13,7 @@ public class WOBuddyActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-		startActivityForResult(intent, 0);
 
-	}
-
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == 0) {
-			if (resultCode == RESULT_OK) {
-				String contents = intent.getStringExtra("SCAN_RESULT");
-				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-				// Handle successful scan
-			} else if (resultCode == RESULT_CANCELED) {
-				// Handle cancel
-			}
-		}
 	}
 
 }
