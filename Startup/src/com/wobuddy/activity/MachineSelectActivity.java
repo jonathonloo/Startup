6227b1f9d9121597576d64092b7ddf3df45fd6cc
Jonathon Loo <jloo@referentia.com>
2012-04-28 19:25:20 -1000

    tweak

--- Startup/src/com/wobuddy/activity/MachineSelectActivity.java
+++ Startup/src/com/wobuddy/activity/MachineSelectActivity.java
@@ -18,7 +18,7 @@ public class MachineSelectActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.machineselect);
+		setContentView(R.layout.machinelist);
 	}
 
 	@Override
